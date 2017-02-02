b907bdede0fada349b7ace25bed05448f55e1648
陶梦迪 <taomengdi99@163.com>
2017-02-02 10:21:59 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts '建立四个groups'
+Group.create!(title: '天天用英语社群', description:'这里每天都要学习英语')
+Group.create!(title: '原则 英语原版书阅读', description:'	prinsiples，你值得阅读')
+Group.create!(title:'财富自由之路', description:'李笑来专栏，推荐去看')
+Group.create!(title:'筋络', description:'七经八脉')
